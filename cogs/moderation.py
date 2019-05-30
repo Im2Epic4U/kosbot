e2d6cf3c2c248a826dbbf6f6a2d0f9a40e3e0668
Im2Epic4U <unconfigured@null.spigotmc.org>
2019-05-30 14:02:00 -0500

    bug fix

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -16,7 +16,7 @@ class Moderation(commands.Cog):
             embed = discord.Embed(description=f'Kicked **{member.mention}** for **{reason}**', colour=discord.Colour.green())
             await ctx.send(content=None, embed=embed)
         else:
-            embed = discord.Embed(description=f"You don't have permission to execute this command, {ctx.author.mention}!")
+            embed = discord.Embed(description=f"You don't have permission to execute this command, {ctx.author.mention}!", colour=discord.Colour.red())
             await ctx.send(content=None, embed=embed)
             
 
@@ -24,19 +24,20 @@ class Moderation(commands.Cog):
     async def unban(self, ctx, *, member):
         banned_users = await ctx.guild.bans()
         member_name, member_discriminator = member.split('#')
-
+        
+        
         for ban_entry in banned_users:
             user = ban_entry.user
 
-            if Staff in [role.id for role in ctx.author.roles]:
-                if (user.name, user.discriminator) == (member_name, member_discriminator):
-                    await ctx.guild.unban(user)
-                    embed = discord.Embed(description=f'Unbanned **{user.mention}**', colour=discord.Colour.green())
-                    await ctx.send(content=None, embed=embed)
-                    return
-            else:
-                embed = discord.Embed(description=f"You don't have permission to execute this command, {ctx.author.mention}!", colour=discord.Colour.red())
+        if Staff in [role.id for role in ctx.author.roles]:
+            if (user.name, user.discriminator) == (member_name, member_discriminator):
+                await ctx.guild.unban(user)
+                embed = discord.Embed(description=f'Unbanned **{user.mention}**', colour=discord.Colour.green())
                 await ctx.send(content=None, embed=embed)
+                return
+        else:
+            embed = discord.Embed(description=f"You don't have permission to execute this command, {ctx.author.mention}!", colour=discord.Colour.red())
+            await ctx.send(content=None, embed=embed)
 
     @commands.command()
     async def ban(self, ctx, member: discord.Member, *, reason=None):
